32eaa6107fb80f695b5ace48b6b12799cf6dae41
Chris Hoffman <chrishoff143@gmail.com>
2021-03-03 22:14:50 -0600

    Staff View
    Added views for what a staff member will see when they log in. Will need to be routed to upon a staff member logging in, but for now it can be seen by adding /Staff to the url after running the program.

---
+++ ymca-application/Controllers/StaffController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ymca_application.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: Staff
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Programs()
+        {
+            return View();
+        }
+
+        public ActionResult Staff()
+        {
+            return View();
+        }
+
+        public ActionResult Members()
+        {
+            return View();
+        }
+
+        public ActionResult Schedule()
+        {
+            return View();
+        }
+
+        public ActionResult AccountInfo()
+        {
+            return View();
+        }
+
+        public ActionResult EditPrograms()
+        {
+            return View();
+        }
+    }
+}
